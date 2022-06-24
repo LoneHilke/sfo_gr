a3e6aaa6f1fdd872160b077fe91ab6f55f09d33b
Lone Hilke <lonehilke@gmail.com>
2022-06-24 22:31:57 +0200

    færdig
    færdig
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- myworld/members/models.py
+++ myworld/members/models.py
@@ -1,10 +1,11 @@
+from statistics import mode
 from django.db import models
 
 # Create your models here.
 class Members(models.Model):
-    gruppe = models.ManyToManyField('gruppe', blank=True, related_name='items')
+    gruppe = models.TextField(blank=True)
     name = models.CharField(max_length=255)
     modul = models.CharField(max_length=255)
 
-class Gruppe(models.Model):
-    name = models.CharField(max_length=255)
+#class Gruppe(models.Model):
+    #name = models.CharField(max_length=255)
--- myworld/members/views.py
+++ myworld/members/views.py
@@ -1,8 +1,53 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
+from .models import Members
 
 # Create your views here.
 def index(request):
-    template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    mymembers = Members.objects.all().values()
+    template = loader.get_template('index.html')
+    context = {
+        'mymembers': mymembers
+    }
+    return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['name']
+  y = request.POST['modul']
+  z = request.POST['gruppe']
+  member = Members(name=x, modul=y, gruppe=z)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  member = Members.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+  mymember = Members.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+  name = request.POST['name']
+  modul = request.POST['modul']
+  gruppe = request.POST['gruppe']
+  member = Members.objects.get(id=id)
+  member.name = name
+  member.modul = modul
+  member.gruppe = gruppe
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+
+#fra: https://www.w3schools.com/django/django_update_record.php
