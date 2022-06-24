8ee646ab79cc552a8f616ebe6aedfe34f02c7c2b
Lone Hilke <lonehilke@gmail.com>
2022-06-24 23:40:39 +0200

    ugedage
    nu med ugedage (hvis det vil virke
    
    Co-Authored-By: Qvisty <18654175+qvisty@users.noreply.github.com>

--- myworld/members/models.py
+++ myworld/members/models.py
@@ -6,6 +6,12 @@ class Members(models.Model):
     gruppe = models.TextField(blank=True)
     name = models.CharField(max_length=255)
     modul = models.CharField(max_length=255)
+    mandag = models.CharField(blank=True, max_length=255)
+    tirsdag = models.CharField(blank=True, max_length=255)
+    onsdag = models.CharField(blank=True, max_length=255)
+    torsdag = models.CharField(blank=True, max_length=255)
+    fredag = models.CharField(blank=True, max_length=255)
+
 
 #class Gruppe(models.Model):
     #name = models.CharField(max_length=255)
